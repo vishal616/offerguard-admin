db36bb08c1755f830698315df831cfa8bef86e3d
Vishal <vishal.vishalmishra.mishra2@gmail.com>
2021-08-31 20:46:44 +0530

    adds ag grid functionalities

--- src/components/offer-list/OfferList.tsx
+++ src/components/offer-list/OfferList.tsx
@@ -10,24 +10,52 @@ function OfferList() {
 	const columnDefs = [
 		{ headerName: 'Offer Id', field: 'offerid' },
 		{ headerName: 'Name', field: 'name' },
-		{ headerName: 'Os', field: 'os_allow'},
-		{ headerName: 'Country', field: 'country_allow'},
 		{ headerName: 'Affiliate Status', field: 'affiliateStatus'},
-		{ headerName: 'Redirects', field: 'redirects'},
+		{ headerName: 'Redirects', field: 'redirects', filter: 'agNumberColumnFilter'},
+		{ headerName: 'Os Allowed', field: 'os_allow'},
+		{ headerName: 'Os Blocked', field: 'os_block'},
+		{ headerName: 'Country Allowed', field: 'country_allow'},
+		{ headerName: 'Country Blocked', field: 'country_block'},
+		{ headerName: 'Click Url', field: 'click_url'},
+		{ headerName: 'Impression Url', field: 'impression_url'},
+		{ headerName: 'Category', field: 'category'},
+		{ headerName: 'Currency', field: 'currency'},
+		{ headerName: 'Price', field: 'price', filter: 'agNumberColumnFilter'},
+		{ headerName: 'Model', field: 'model'},
+		{ headerName: 'Start Date', field: 'date_start'},
+		{ headerName: 'End Date', field: 'end_start'},
+		{ headerName: 'Preview Url', field: 'preview_url'},
+		{ headerName: 'City Allowed', field: 'city_allow'},
+		{ headerName: 'City Blocked', field: 'city_block'},
+		{ headerName: 'Device Allowed', field: 'device_allow'},
+		{ headerName: 'Device Blocked', field: 'device_block'},
+		{ headerName: 'ISP Allowed', field: 'isp_allow'},
+		{ headerName: 'ISP Blocked', field: 'isp_block'}
 	];
 
 	useEffect( () => {
 		OfferService.getAllOffers().then(({data}) => {
 			setRowData(data);
+
 		})
 	}, []);
 
+	const openOfferModal = (offer: any) => {
+		console.log(offer);
+	};
+
 	return (
-		<div className="offer-list">
-			<div className="ag-theme-alpine" style={{height: 800, width: 800}}>
+		<div className="offers">
+			<div className="ag-theme-alpine offer-list">
 				<AgGridReact
 					columnDefs={columnDefs}
 					rowData={rowData}
+					enableCellTextSelection={true}
+					enableBrowserTooltips={true}
+					floatingFilter={true}
+					animateRows={true}
+					defaultColDef={{sortable: true, filter: true, resizable: true}}
+					onCellDoubleClicked={openOfferModal}
 				></AgGridReact>
 			</div>
 		</div>
--- src/services/offerService.ts
+++ src/services/offerService.ts
@@ -5,4 +5,8 @@ export class OfferService {
 	static async getAllOffers(apiUrl?: string) {
 		return axios.get('http://localhost:8080/api/offers/').then((response) => response).catch((error) => error);
 	}
+
+	static async getAllRedirectUrlsForOffer(offerId?: string) {
+		return axios.get('http://localhost:8080/api/redirects/offers/' + offerId).then((response) => response).catch((error) => error);
+	}
 }
