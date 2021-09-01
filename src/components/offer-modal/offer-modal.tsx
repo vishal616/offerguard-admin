168d31db1cf7375dec9b1169636997e9e27374e6
Vishal <vishal.vishalmishra.mishra2@gmail.com>
2021-09-01 23:09:31 +0530

    adds modal style and info

--- src/components/offer-modal/offer-modal.tsx
+++ src/components/offer-modal/offer-modal.tsx
@@ -6,12 +6,24 @@ import './offer-modal.css';
 function OfferModal(props: any) {
 	const [open, setOpen] = useState(false);
 	const [redirectUrls, setRedirectUrls] = useState([]);
+	const [offer, setOffer] = useState<Offer>({
+		name: '',
+		country: '',
+		id: '',
+		os: ''
+	});
 
 	useEffect(() => {
 		if(props.data) {
 			setOpen(true);
 			RedirectUrlService.getAllRedirectUrlsForOffer(props.data.offerid).then(({data}) => {
 				const urls = data.map((data: any) => data.url);
+				setOffer({
+					name: props.data.name,
+					id: props.data.offerid,
+					os: props.data.os_allow,
+					country: props.data.country_allow
+				});
 				setRedirectUrls(urls);
 			});
 		}
@@ -22,16 +34,28 @@ function OfferModal(props: any) {
 			<ReactModal
 				isOpen={open}
 			>
-				<button onClick={() => setOpen(false)}>close</button>
+				<i className="fa fa-times" aria-hidden="true" onClick={() => setOpen(false)}></i>
 				<h2>Offer Redirections</h2>
+				<div><strong>Id : </strong>{offer.id}</div>
+				<div><strong>Name : </strong>{offer.name}</div>
+				<div><strong>Country : </strong>{offer.country}</div>
+				<div><strong>Os : </strong>{offer.os}</div>
+				<h4>Urls</h4>
 				{
 					redirectUrls.map((url, index) => {
-						return <div key={index}>{url}</div>
+						return <div className="url" key={index}>{url}</div>
 					})
 				}
 			</ReactModal>
 		</div>
 	);
+}
+
+export interface Offer {
+	name: string;
+	id: string;
+	country: string;
+	os: string;
 }
 
 export default OfferModal;
